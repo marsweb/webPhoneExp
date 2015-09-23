74ddda596f6a7d374096fefce97344aa5224e62d
marsweb <marswebb@qq.com>
2015-09-23 16:30:50 +0800

    new version of my phone webpage

--- js/script.js
+++ js/script.js
@@ -7,18 +7,44 @@ for (var j=0; j<resizes.length; j++) {
 	resizes[j].style.top=parseInt(resizes[j].style.top)*scaleH+'px';
 	resizes[j].style.left=parseInt(resizes[j].style.left)*scaleW+'px'; 
 }
-var mySwiper = new Swiper ('.swiper-container', {
-	direction: 'vertical',
-	  //virtualTranslate : true,
-   mousewheelControl : true,
-   onInit: function(swiper){
-   swiperAnimateCache(swiper);
-   swiperAnimate(swiper);
-	  },
-   onSlideChangeEnd: function(swiper){
-	swiperAnimate(swiper);
-    },
-	onTransitionEnd: function(swiper){
-	swiperAnimate(swiper);
-    },
-})
+window.onload = function() {
+	var mySwiper = new Swiper ('.swiper-container', {
+		direction: 'vertical',
+		loop: true,
+		pagination: '.swiper-pagination',
+		//virtualTranslate : true,
+		mousewheelControl : true,
+		onInit: function(swiper){
+			swiperAnimateCache(swiper);
+			swiperAnimate(swiper);
+		},
+		onSlideChangeEnd: function(swiper){
+			swiperAnimate(swiper);
+		},
+		onTransitionEnd: function(swiper){
+			swiperAnimate(swiper);
+		},
+		watchSlidesProgress: true,
+		onProgress: function(swiper){
+			for (var i = 0; i < swiper.slides.length; i++){
+				var slide = swiper.slides[i];
+				var progress = slide.progress;
+				var translate = progress*swiper.height/4;  
+				scale = 1 - Math.min(Math.abs(progress * 0.5), 1);
+				var opacity = 1 - Math.min(Math.abs(progress/2),0.5);
+				slide.style.opacity = opacity;
+				es = slide.style;
+				es.webkitTransform = es.MsTransform = es.msTransform = es.MozTransform = es.OTransform = es.transform = 'translate3d(0,'+translate+'px,-'+translate+'px) scaleY(' + scale + ')';
+			}
+		},
+		onSetTransition: function(swiper, speed) {
+			for (var i = 0; i < swiper.slides.length; i++){
+				es = swiper.slides[i].style;
+				es.webkitTransitionDuration = es.MsTransitionDuration = es.msTransitionDuration = es.MozTransitionDuration = es.OTransitionDuration = es.transitionDuration = speed + 'ms';
+			}
+		},
+	});
+}
+function goSection(index){
+	mySwiper.slideTo(index, 1000, false);
+}
